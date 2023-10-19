d4af1fac2d87a06d230ad7230eba294dcc416e32
KingVazz <fazaalhasani1006@gmail.com>
2023-10-19 14:35:09 +0700

    getElementbyId

--- index.js
+++ index.js
@@ -21,3 +21,26 @@ app.put('/estea', (req, res) => {
 app.delete('/estea', (req, res) => {
     res.send('Delete kang')
 });
+
+ // representasi data dari database
+ const data_users = [
+    { id: 1, name: "Azka Dapin", alamat: "Nagarta" },
+    { id: 2, name: "Ghani Rasya", alamat: "Surakarta" },
+    { id: 3, name: "Hana Zain", alamat: "Jakarta" },
+    { id: 4, name: "Ibrahim", alamat: "Bandung" },
+];
+
+app.get("/users", (req, res) => {
+    //mendapatkan data dari database
+    const data = data_users;
+
+    //memberikan respon json data
+    let result = {
+        status: 200,
+        data: data,
+    };
+
+    res.json(result);
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
